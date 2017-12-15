0a27c5863db2921cad36116319ec3e2204acf8b2
Andrey Susoev <andwow@gmail.com>
2017-12-15 15:41:14 +0300

    install scripts for ruby & mongodb

---
+++ homework06/runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Functions for script execution
+
+# Execute pack of commands
+function execute_cmd_list {
+    local i=0
+    filename=$1;
+    if [[ -f "$filename" ]]; then
+        echo "Command list file: $filename doest not exists"
+        return 1
+    fi
+    while IFS=";\n" read cmd && [[ -n "$cmd" ]]; do
+        $cmd
+        local rc=$?
+        if [[ "$rc" != 0 ]]; then
+            echo "Command <$cmd> failed. Installation stopped"
+            return 1
+        fi
+    done < "$filename"
+    return 0
+}
+
+
+execute_cmd_list ruby.txt
+exit $?
