b35f6eae700f821b2088908c061854279b6d6f0e
Andrey Susoev <andwow@gmail.com>
2017-12-15 17:22:20 +0300

    runner whitespace \ fix

--- homework06/runner.sh
+++ homework06/runner.sh
@@ -8,14 +8,14 @@ function execute_cmd_list {
         echo "Command list file: $filename doest not exists"
         return 1
     fi
-    while IFS= read -r cmd && [[ -n "$cmd" ]]; do
+    while IFS= read cmd && [[ -n "$cmd" ]]; do
         $cmd
         local rc=$?
         if [[ "$rc" != 0 ]]; then
             echo "Command <$cmd> failed. Installation stopped"
             return 1
         fi
-        done <<< "$(sed ':a;N;$!ba;s/\\\n//g' $filename)"
+        done <<< "$(sed ':a;N;$!ba;s/\\\s\n//g' $filename)"
     return 0
 }
 
