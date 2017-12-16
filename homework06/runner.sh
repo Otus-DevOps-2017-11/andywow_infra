3469d1a88b5c65282e641433cbd23a48ff5fe469
Andrey Susoev <andwow@gmail.com>
2017-12-16 19:56:13 +0300

    Back to current dir after script finished

--- homework06/runner.sh
+++ homework06/runner.sh
@@ -4,6 +4,7 @@
 # Execute pack of commands
 function execute_cmd_list {
     local filename=$1;
+    local current_dir=$(realpath $(dirname $0))
     if [[ ! -f "$filename" ]]; then
 	cmdlist_url="$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/cmdlist -H 'Metadata-Flavor: Google')"
 	wget -O "/tmp/startup.txt" "$cmdlist_url"
@@ -22,10 +23,12 @@ function execute_cmd_list {
             echo "Command <$cmd> failed. Installation stopped"
             return 1
         fi
-        done <<< "$(sed ':a;N;$!ba;s/\\\s\n//g' $filename)"
+    done <<< "$(sed ':a;N;$!ba;s/\\\s\n//g' $filename)"
+    # back to current directory
+    cd $current_dir
     return 0
 }
 
-
 execute_cmd_list $1
 exit $?
+
