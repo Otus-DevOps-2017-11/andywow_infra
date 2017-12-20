e34acb86f562d4f897f6e4fd5181a44b1f2af70c
Andrey Susoev <andwow@gmail.com>
2017-12-21 00:34:00 +0300

    deploy.sh script

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+cd /opt
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+cd reddit && bundle install
+echo "[Unit]
+Description=Puma service
+After=syslog.target network.target
+
+[Service]
+Type=simple
+WorkingDir=/opt/reddit
+ExecStart=/usr/local/bin/puma
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/puma.service
+systemctl daemon-reload
+systemctl start puma
+systemctl enable puma
+ps aux | grep puma
