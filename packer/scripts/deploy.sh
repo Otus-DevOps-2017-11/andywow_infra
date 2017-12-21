c0e6cb87e236de2373d5992b1287400a94aff51c
Andrey Susoev <andwow@gmail.com>
2017-12-21 18:57:14 +0300

    systemd unit separate dir

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -4,18 +4,7 @@ set -e
 cd /opt
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 cd reddit && bundle install
-echo "[Unit]
-Description=Puma service
-After=syslog.target network.target
-
-[Service]
-Type=simple
-WorkingDirectory=/opt/reddit
-ExecStart=/usr/local/bin/puma
-
-[Install]
-WantedBy=multi-user.target
-" > /etc/systemd/system/puma.service
+mv /tmp/puma.service /etc/systemd/system/puma.service
 systemctl daemon-reload
 systemctl start puma
 systemctl enable puma
