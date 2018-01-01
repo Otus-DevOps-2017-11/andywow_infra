09eaf0dbea31aa114d6519a0cedb1a6af908c7ea
Andrey Susoev <andwow@gmail.com>
2018-01-01 16:27:01 +0300

    loadbalancer addr ouput

--- terraform/code/outputs.tf
+++ terraform/code/outputs.tf
@@ -5,3 +5,7 @@ output "app_external_ip" {
 output "db_external_ip" {
   value = "${module.db.db_external_ip}"
 }
+
+output "load_balancer_ip" {
+  value = "${module.load_balancer.load_balancer_ip}"
+}
