ea866f5e93d4852a3bf2c0101c2300ec0aebc300
Andrey Susoev <andwow@gmail.com>
2018-01-21 01:52:00 +0300

    db test - socket is listening

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -17,3 +17,8 @@ def test_config_file(File):
     config_file = File('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+# check if 27017 port is listening
+def test_mongo_port_is_listening(host):
+    socket = host.socket("tcp://0.0.0.0:27017")
+    assert socket.is_listening
